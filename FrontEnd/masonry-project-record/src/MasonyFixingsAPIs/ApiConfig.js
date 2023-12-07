5738322b00ed3980cc75e49ea3522584db7cf6d5
David Carroll <carrold8@tcd.ie>
2023-12-07 13:16:56 +0000

    Changed Base URL in ApiConfig.js

--- FrontEnd/masonry-project-record/src/MasonyFixingsAPIs/ApiConfig.js
+++ FrontEnd/masonry-project-record/src/MasonyFixingsAPIs/ApiConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    // baseURL: 'http://localhost:8080',
+    baseURL: 'ec2-3-254-137-159.eu-west-1.compute.amazonaws.com',
     timeout: 20000
 })
