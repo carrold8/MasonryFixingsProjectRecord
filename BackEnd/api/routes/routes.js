efebb273f6e5c7f72a066733d8c7e57303f92dde
David Carroll <carrold8@tcd.ie>
2023-12-07 13:50:03 +0000

    Changed origin to http://localhost:8080

--- BackEnd/api/routes/routes.js
+++ BackEnd/api/routes/routes.js
@@ -32,7 +32,7 @@ const { Project } = require('../models/project.model');
 module.exports = function(app) {
 
   var corsOptions = {
-    origin: '/*'
+    origin: 'http://localhost:8080'
 }
   app.use(cors(corsOptions));
 
