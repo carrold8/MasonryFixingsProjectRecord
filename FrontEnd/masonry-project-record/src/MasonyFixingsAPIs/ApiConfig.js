24dd448780ae13bd42a68373f18b512d90a7525d
David Carroll <carrold8@tcd.ie>
2023-12-07 15:01:30 +0000

    Changed to 3.254.137.159

--- FrontEnd/masonry-project-record/src/MasonyFixingsAPIs/ApiConfig.js
+++ FrontEnd/masonry-project-record/src/MasonyFixingsAPIs/ApiConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    // baseURL: 'http://localhost:8080',
+    baseURL: 'http://3.254.137.159',
     // baseURL: 'ec2-3-254-137-159.eu-west-1.compute.amazonaws.com',
     timeout: 20000
 })
