93589d461aa90913a98c99756ab0950bff6e5225
David Carroll <carrold8@tcd.ie>
2023-12-19 05:39:32 +0000

    Changed logo and added .env to backend

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -1,7 +1,18 @@
 
 
 const dbConfig = require('../config/db.config');
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_DATABASE = process.env.DB_DATABASE
+const DB_DIALECT = process.env.DB_DIALECT
+const DB_POOL_MAX = process.env.DB_POOL_MAX
+const DB_POOL_MIN = process.env.DB_POOL_MIN
+const DB_ACQUIRE = process.env.DB_ACQUIRE
+const DB_IDLE = process.env.DB_IDLE
 
 
 // const sequelize = new Sequelize(dbConfig.config.DB, dbConfig.config.USER,
@@ -17,16 +28,31 @@ const { Sequelize } = require('sequelize')
 //         }
 //     })
 
-const sequelize = new Sequelize(dbConfig.configCloud.DB, dbConfig.configCloud.USER,
-    dbConfig.configCloud.PASSWORD, {
-        host: dbConfig.configCloud.HOST,
-        dialect: dbConfig.configCloud.dialect,
+// const sequelize = new Sequelize(dbConfig.configCloud.DB, dbConfig.configCloud.USER,
+//     dbConfig.configCloud.PASSWORD, {
+//         host: dbConfig.configCloud.HOST,
+//         dialect: dbConfig.configCloud.dialect,
+//         operationsAliases: false,
+//         pool: {
+//         max: dbConfig.configCloud.pool.max,
+//         min: dbConfig.configCloud.pool.min,
+//         acquire: dbConfig.configCloud.pool.acquire,
+//         idle: dbConfig.configCloud.pool.idle
+//         }
+//         })
+
+const sequelize = new Sequelize(
+    DB_DATABASE, 
+    DB_USER,
+    DB_PASSWORD, {
+        host: DB_HOST,
+        dialect: 'mysql',
         operationsAliases: false,
         pool: {
-        max: dbConfig.configCloud.pool.max,
-        min: dbConfig.configCloud.pool.min,
-        acquire: dbConfig.configCloud.pool.acquire,
-        idle: dbConfig.configCloud.pool.idle
+        max: DB_POOL_MAX,
+        min: DB_POOL_MIN,
+        acquire: DB_ACQUIRE,
+        idle: DB_IDLE
         }
         })
 
