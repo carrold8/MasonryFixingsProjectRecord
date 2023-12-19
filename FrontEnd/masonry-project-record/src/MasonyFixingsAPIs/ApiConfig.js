72f9dae7d2b55c790e2607ca429b2da860ce3667
David Carroll <carrold8@tcd.ie>
2023-12-19 07:53:46 +0000

    Added full address as base url

--- FrontEnd/masonry-project-record/src/MasonyFixingsAPIs/ApiConfig.js
+++ FrontEnd/masonry-project-record/src/MasonyFixingsAPIs/ApiConfig.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: 'http://localhost:8080/api',
-    baseURL: 'https://3.254.137.159/api',
+    // baseURL: 'http://3.254.137.159/api',
+    baseURL: 'https://masonryfixingsprojects.com/api',
     timeout: 20000
 })
