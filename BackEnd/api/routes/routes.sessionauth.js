aa3c03f2aa8cf68466f9ecc76af3accdf4050cf1
David Carroll <carrold8@tcd.ie>
2024-01-12 05:33:40 +0000

    Looking for user session in userAuth now

--- BackEnd/api/routes/routes.sessionauth.js
+++ BackEnd/api/routes/routes.sessionauth.js
@@ -2,12 +2,11 @@ const express = require('express');
 
 exports.userAuth = (req, res, next) => {
     console.log('got here');
-    const session = req.session;
-    console.log('Session: ', session)
-    if(session){
+    const sessionUser = req.session.user;
+    console.log('Session: ', sessionUser)
+    if(sessionUser){
         console.log('session found');
         next();
-        
     }
     else{
         return res.status(401).send('Unauthorised Request')
