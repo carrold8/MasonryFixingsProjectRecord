ff7538bc739d41cf46618743d6cba456780e9224
David Carroll <carrold8@tcd.ie>
2024-01-12 05:27:30 +0000

    Changed import of userAuth

--- BackEnd/api/routes/routes.sessionauth.js
+++ BackEnd/api/routes/routes.sessionauth.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
 exports.userAuth = (req, res, next) => {
-    const session = req.session;
-    console.log('Session: ', session)
-    if(session){
-        console.log('session found');
-        next();
+    console.log('got here');
+    // const session = req.session;
+    // console.log('Session: ', session)
+    // if(session){
+    //     console.log('session found');
+    //     next();
         
-    }
-    else{
-        return res.status(401).send('Unauthorised Request')
-    }
+    // }
+    // else{
+    //     return res.status(401).send('Unauthorised Request')
+    // }
 }
