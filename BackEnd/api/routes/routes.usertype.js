ba834b204d80a9747d252f0fa651ee4f724b71ac
David Carroll <carrold8@tcd.ie>
2024-01-12 08:19:55 +0000

    Reverted usertype api

--- BackEnd/api/routes/routes.usertype.js
+++ BackEnd/api/routes/routes.usertype.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const {userAuth, managementAuth} = require('./routes.sessionauth');
 
 router.get('/', userAuth, function(request, response) {
-    response.json({management: true});
+    const userRole = request.session.user.role;
+    console.log('Role: ', userRole);
+    if(userRole === 'Sales'){
+        response.json({management: false});
+    }
+    else{
+        response.json({management: true});
+    }
 });
 
 module.exports = router;
