7509ae0f6fe5bc30a9a1bf660ec61563d315ec4e
David Carroll <carrold8@tcd.ie>
2024-01-12 08:12:36 +0000

    Added managementAuth and edited usertype API

--- BackEnd/api/routes/routes.sessionauth.js
+++ BackEnd/api/routes/routes.sessionauth.js
@@ -9,3 +9,18 @@ exports.userAuth = (req, res, next) => {
         return res.status(401).send('Unauthorised Request')
     }
 }
+
+exports.managementAuth = (req, res, next) => {
+    const sessionUser = req.session.user;
+    if(sessionUser){
+        if(sessionUser.role === 'Sales'){
+            res.status(401).send('Unauthorised request. Wrong role')
+        }
+        else{
+            next();
+        }
+    }
+    else{
+        return res.status(401).send('Unauthorised Request')
+    }
+}
--- BackEnd/api/routes/routes.usertype.js
+++ BackEnd/api/routes/routes.usertype.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {userAuth} = require('./routes.sessionauth');
+const {userAuth, managementAuth} = require('./routes.sessionauth');
 
-router.get('/', userAuth, function(request, response) {
-    const userRole = request.session.user.role;
-    console.log('Role: ', userRole);
-    if(userRole === 'Sales'){
-        response.json({management: false});
-    }
-    else{
-        response.json({management: true});
-    }
+router.get('/', managementAuth, function(request, response) {
+    response.json({management: true});
 });
 
 module.exports = router;
