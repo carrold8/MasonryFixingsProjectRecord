16f6ed71491531997c78ee793dc6828c0f307960
David Carroll <carrold8@tcd.ie>
2024-01-12 05:31:26 +0000

    checking if session can be checked in county

--- BackEnd/api/routes/routes.sessionauth.js
+++ BackEnd/api/routes/routes.sessionauth.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 exports.userAuth = (req, res, next) => {
     console.log('got here');
-    // const session = req.session;
-    // console.log('Session: ', session)
-    // if(session){
-    //     console.log('session found');
-    //     next();
+    const session = req.session;
+    console.log('Session: ', session)
+    if(session){
+        console.log('session found');
+        next();
         
-    // }
-    // else{
-    //     return res.status(401).send('Unauthorised Request')
-    // }
+    }
+    else{
+        return res.status(401).send('Unauthorised Request')
+    }
 }
